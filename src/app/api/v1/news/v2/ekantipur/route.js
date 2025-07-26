4781d824818dd42b9a6142faaa3c45cc382461fe
Utsab Adhikari <utsab1adhikari@gmail.com>
2025-07-26 11:52:29 +0545

    updated

--- src/app/api/v1/news/v2/ekantipur/route.js
+++ src/app/api/v1/news/v2/ekantipur/route.js
@@ -9,6 +9,10 @@ export async function GET(request) {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9",
+        Referer: "https://www.google.com/",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        Connection: "keep-alive",
       },
     });
     const html = response.data;
